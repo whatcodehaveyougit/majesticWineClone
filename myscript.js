9dfa68b26698e0ae43c939adebb8809c153a49bd
whatcodehaveyougit <siggywatt@yahoo.com>
2018-04-13 13:10:43 +0100

    hello

---
+++ myscript.js
@@ -0,0 +1,21 @@
+
+
+    function changeit() {
+        document.getElementById("pinkback").innerHTML = "This is a change";
+    }
+
+    // function change(changeColor) {
+    //   changeColor.style.color = "white";
+    //   changeColor.style.backgroundColor = "#b61364";
+    // }
+
+    // function changeback(changeColor) {
+    //   changeColor.style.color = "#b61364";
+    //   changeColor.style.backgroundColor = "white";
+    // }
+
+
+    // function chango() {
+    //   document.getElementsById("test").style.color = "blue";
+    //   document.getElementsByClassName("survey_cta").style.backgroundColor = "red";
+    // }
